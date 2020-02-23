968c0cb960d4e1addb4264397a9a00e828ca6240
Dillon McGovern <dillonm800@gmail.com>
2020-02-23 14:11:44 -0500

    test for valid config gile

--- server/config/express.js
+++ server/config/express.js
@@ -10,7 +10,7 @@ module.exports.init = () => {
         connect to database
         - reference README for db uri
     */
-    mongoose.connect(process.env.DB_URI || require('./config').db.uri, {
+    mongoose.connect(process.env.DB_URI || require('./config.example').db.uri, {
         useNewUrlParser: true
     });
     mongoose.set('useCreateIndex', true);

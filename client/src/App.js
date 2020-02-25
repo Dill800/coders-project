70715ce2d888f2d8e37aeddc0d399bb975d6f3b1
Dillon McGovern <dillonm800@gmail.com>
2020-02-25 12:42:01 -0500

    created landing page

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,16 @@
 import React from 'react';
-import { Route, Switch, Redirect  } from 'react-router-dom';
+import { Link, Route, Switch, Redirect  } from 'react-router-dom';
 import Home from "./views/Home/Home";
+import Landing from "./views/Landing/Landing";
 import NotFound from "./views/NotFound";
-import NavBar from "./components/Header/NavBar";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
   return (
     <div>
-      <NavBar />
       <Switch>
         <Route exact path="/Home" component={Home} />
-        <Route exact path="/">
-          <Redirect to="/Home" />
-        </Route>
+        <Route exact path="/" component={Landing}/>
         <Route component={NotFound}/>
       </Switch>
     </div>
---
+++ client/src/views/Landing/Landing.js
@@ -0,0 +1,61 @@
+import React, {useState} from 'react'
+import {Button, Container, Col, Row, Alert, Form, Grid, Jumbotron} from 'react-bootstrap'
+import './Landing.css'
+import { Link } from 'react-router-dom';
+
+const Landing = (props) => {
+
+    const [email, setEmail] = useState('');
+    const [pass, setPass] = useState('');
+
+    const submit = (event) => {
+        event.preventDefault();
+        console.log('email: ' + email + " pass: " + pass);
+    }
+
+    const onEmailChange = (event) => {
+        setEmail(event.target.value);
+    }
+
+    const onPassChange = (event) => {
+        setPass(event.target.value);
+    }
+
+    return (
+        <Container>
+            <Row className='justify-content-center'>
+            <Col xs={9} md={6}>
+
+            <div className='landing-title-container'>
+            <h1 className='landing-title'>Sign In</h1>
+            
+            </div>
+
+            <Form onSubmit={submit}>
+            <Form.Group controlId="formBasicEmail">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control onChange={onEmailChange} required type="email" placeholder="Enter email" />
+            </Form.Group>
+
+            <Form.Group controlId="formBasicPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control onChange={onPassChange} required type="password" placeholder="Password" />
+            </Form.Group>
+            
+            <Button block variant="outline-primary" type="submit">
+                Login
+            </Button>
+            </Form>
+            </Col>
+            </Row>
+
+            <div className='forgot-password-container'>
+                <Link>Forgot Password?</Link>
+            </div>
+
+        </Container>
+    )
+
+}
+
+export default Landing;

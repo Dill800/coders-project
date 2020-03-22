e90203e04339e1a44b6512a73444fcf9a17ec21b
Bryan Delgado <bryan.delgado@ufl.edu>
2020-03-22 17:56:51 -0400

    commit

---
+++ client/src/views/Quiz/Quiz.js
@@ -0,0 +1,10 @@
+import React, {useState} from 'react'
+import {Alert, Button, Container, Col, Row, Form,} from 'react-bootstrap'
+import './Quiz.css'
+
+const Quiz = (props) => {
+    
+}
+
+export default Quiz;
+

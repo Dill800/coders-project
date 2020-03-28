e81837615e606e16436b23638b5939360371600b
Dillon McGovern <dillonm800@gmail.com>
2020-03-28 14:26:53 -0400

    fixed heroku config deploy issue

--- server/controllers/weatherController.js
+++ server/controllers/weatherController.js
@@ -1,5 +1,7 @@
 const axios = require('axios')
-const config = require('../config/config')
+
+const darkSkyKey = process.env.darkSkyKey || require('../config/config').darkSkyApiKey;
+const openCageKey = process.env.openCageKey || require('../config/config').openCageKey;
 
 module.exports = {
 
@@ -7,7 +9,7 @@ module.exports = {
 
         // /key/lat/long/2018-10-24T19:06:32
 
-        let result = await axios.get('https://api.darksky.net/forecast/' + config.darkSkyApiKey + '/' + req.latitude + ',' + req.longitude + ',' + req.query.date + 'T12:06:32')
+        let result = await axios.get('https://api.darksky.net/forecast/' + darkSkyKey + '/' + req.latitude + ',' + req.longitude + ',' + req.query.date + 'T12:06:32')
 
         res.send({
             city: req.query.city,
@@ -31,7 +33,7 @@ module.exports = {
 
         //https://api.opencagedata.com/geocode/v1/json?q=Gainesville%2C%20Florida&key=20e0b22d3e504d529a4bcb34edf99542&language=en&pretty=1
 
-        let result = await axios.get('https://api.opencagedata.com/geocode/v1/json?q=' + req.query.city + '%2C%20' + req.query.state + '&key=' + config.openCageKey + '&language=en&pretty=1')
+        let result = await axios.get('https://api.opencagedata.com/geocode/v1/json?q=' + req.query.city + '%2C%20' + req.query.state + '&key=' + openCageKey + '&language=en&pretty=1')
 
         req.latitude = result.data.results[0].geometry.lat;
         req.longitude = result.data.results[0].geometry.lng;

be81f6e7d898177bb7095d1a80e10b326f5700ca
David Wang <david220wang385@gmail.com>
2020-04-16 13:28:37 -0400

    Added address form and dashcam checkbox to Register page, also added all 50 states to states dropdown menu, tested and works with db

--- client/src/views/Register/Register.js
+++ client/src/views/Register/Register.js
@@ -8,9 +8,11 @@ const Register = (props) => {
 
     const [email, setEmail] = useState('');
     const [pass, setPass] = useState('');
+    const [address, setAddress] = useState('');
     const [city, setCity] = useState('');
     const [state, setState] = useState('');
     const [isAdmin, setAdmin] = useState(false);
+    const [dashcam, setDashcam] = useState(false);
 
     const submit = (event) => {
         event.preventDefault();
@@ -18,9 +20,11 @@ const Register = (props) => {
 
           email: email,
           passwordHash: pass,
+          address: address,
           city: city,
           state: state,
           accessLevel: isAdmin ? 1 : 0,
+          dashcamInCar : dashcam ? 1 : 0,
           stars: 0
 
         }).then(response => {
@@ -39,20 +43,24 @@ const Register = (props) => {
     const onEmailChange = (event) => {
         setEmail(event.target.value);
     }
-
     const onPassChange = (event) => {
         setPass(event.target.value);
     }
-    
+    const onAddressChange = (event) => {
+      setAddress(event.target.value);
+  }
     const onCityChange = (event) => {
         setCity(event.target.value);
     }
     const onStateChange = (event) => {
         setState(event.target.value);
     }
-    const onChecked = event => {
+    const onAdminChecked = event => {
         setAdmin(event.target.checked)
     }
+    const onDashCamChecked = event => {
+      setDashcam(event.target.checked)
+  }
 
     return (
         <Container>
@@ -65,6 +73,8 @@ const Register = (props) => {
             </div>
 
             <Form onSubmit={submit}>
+
+{/* Forms Boxes */}
 <Form.Row>
     <Form.Group as={Col} controlId="formGridEmail">
       <Form.Label>Email</Form.Label>
@@ -78,6 +88,12 @@ const Register = (props) => {
     </Form.Group>
 </Form.Row>
 <Form.Row>
+    <Form.Group as={Col} controlId="formGridAddress">
+      <Form.Label>Address</Form.Label>
+      <Form.Control onChange={onAddressChange} required type="address" placeholder="Address" />
+    </Form.Group>
+</Form.Row>
+<Form.Row>
     <Form.Group as={Col} controlId="formGridCity">
       <Form.Label>City</Form.Label>
       <Form.Control onChange={onCityChange} required type="city" placeholder="City" />
@@ -88,22 +104,82 @@ const Register = (props) => {
       <Form.Label>State</Form.Label>
       <Form.Control required as="select" onChange={onStateChange}>
         <option>Choose...</option>
+        <option>Alabama</option>
+        <option>Alaska</option>
+        <option>Arizona</option>
+        <option>Arkansas</option>
+        <option>California</option>
+        <option>Colorado</option>
+        <option>Connecticut</option>
+        <option>Delaware</option>
         <option>Florida</option>
+        <option>Georgia</option>
+        <option>Hawaii</option>
+        <option>Idaho</option>
+        <option>Illinois</option>
+        <option>Indiana</option>
+        <option>Iowa</option>
+        <option>Kansas</option>
+        <option>Kentucky</option>
+        <option>Louisiana</option>
+        <option>Maine</option>
+        <option>Maryland</option>
+        <option>Massachusetts</option>
+        <option>Michigan</option>
+        <option>Minnesota</option>
+        <option>Mississippi</option>
+        <option>Missouri</option>
+        <option>Montana</option>
+        <option>Nebraska</option>
+        <option>Nevada</option>
+        <option>New Hampshire</option>
+        <option>New Jersey</option>
+        <option>New Mexico</option>
+        <option>New York</option>
+        <option>North Carolina</option>
+        <option>North Dakota</option>
+        <option>Ohio</option>
+        <option>Oklahoma</option>
+        <option>Oregon</option>
+        <option>Pennsylvania</option>
+        <option>Rhode Island</option>
+        <option>South Carolina</option>
+        <option>South Dakota</option>
+        <option>Tennessee</option>
+        <option>Texas</option>
+        <option>Utah</option>
+        <option>Vermont</option>
+        <option>Virginia</option>
+        <option>Washington</option>
+        <option>West Virginia</option>
+        <option>Wisconsin</option>
+        <option>Wyoming</option>
       </Form.Control>
     </Form.Group> 
-  </Form.Row>
+</Form.Row>
 
-  <div >
+{/* Checkboxes */}
+  <div>
   <Form.Group className="inline" id="formGridCheckbox">
     <Form.Check type="checkbox" label="I agree with the"/>
     <a href=" ">&nbsp;terms and conditions.</a>
   </Form.Group>
   </div>
-    <div>
+
+  <div>
   <Form.Group className="inline" id="formGridCheckbox">
-    <Form.Check onChange={onChecked} type="checkbox" label="I am an insurance company"/>
+    <Form.Check onChange={onAdminChecked} type="checkbox" label="I am an insurance company"/>
   </Form.Group>
   </div>
+
+  <div>
+  <Form.Group className="inline" id="formGridCheckbox">
+    <Form.Check onChange={onDashCamChecked} type="checkbox" label="I have a dashcam in my car"/>
+  </Form.Group>
+  </div>
+
+
+  {/* Submit Button */}
   <Form.Group className='button'>
   <Button variant="primary" type="submit">
     Register
@@ -111,12 +187,12 @@ const Register = (props) => {
   </Form.Group>
 </Form>
 <div className='already-have-an-account-container'>
-                <Link to='/'>Already Have an Account?</Link>
-            </div>
-            </Col>
-            </Row>
+    <Link to='/'>Already Have an Account?</Link>
+</div>
 
-        </Container>
+    </Col>
+    </Row>
+    </Container>
     )
 
 }

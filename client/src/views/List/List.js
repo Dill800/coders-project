f1d504447b52ae2cb4133b244bb4e914eb5cd43f
G. Austin Simon <austin@austinsimon.com>
2020-04-20 17:30:41 -0400

    Fixed global style bug

--- client/src/views/List/List.js
+++ client/src/views/List/List.js
@@ -1,9 +1,9 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import UserList from './Components/UserList';
 import ViewUsers from './Components/ViewUsers';
 import { Link, Redirect } from 'react-router-dom';
 import Search from './Components/Search';
-import './List.css';
+// import './List.css';
 //import 'bootstrap/dist/css/bootstrap.min.css';
 import {
 	Navbar,
@@ -17,19 +17,19 @@ import {
 	Button,
 } from 'react-bootstrap';
 
-import axios from 'axios'
-import tokenManager from '../../tokenManager'
+import axios from 'axios';
+import tokenManager from '../../tokenManager';
 
 const List = (props) => {
-    const [filterText, setFilterText] = useState('');
-    const [selectedUser, setSelectedUser] = useState('');
-    const [data, setData] = useState([]);
+	const [filterText, setFilterText] = useState('');
+	const [selectedUser, setSelectedUser] = useState('');
+	const [data, setData] = useState([]);
 
-    useEffect(() => {
-        pullInData()
-    }, [])
+	useEffect(() => {
+		pullInData();
+	}, []);
 
-    // If not signed in or old token or not admin, redirect to login
+	// If not signed in or old token or not admin, redirect to login
 	if (
 		!tokenManager.getCurrentUser() ||
 		tokenManager.getCurrentUser().accessLevel === 0
@@ -37,64 +37,54 @@ const List = (props) => {
 		return <Redirect to='/dashboard'></Redirect>;
 	}
 
-    function pullInData() {
-        axios.get('/users/getUsers')
-        .then(response => {
-            setData(response.data.data)
-        })
-    }
+	function pullInData() {
+		axios.get('/users/getUsers').then((response) => {
+			setData(response.data.data);
+		});
+	}
 
-    const filterUpdate = (value) => {
-        setFilterText(value);
-    };
+	const filterUpdate = (value) => {
+		setFilterText(value);
+	};
 
-    const selectedUpdate = (email) => {
-        setSelectedUser(email);
-    };
+	const selectedUpdate = (email) => {
+		setSelectedUser(email);
+	};
 
+	return (
+		<div className='bg'>
+			<h1>User Directory List</h1>
 
-    return (
-        <div className="bg">
+			<Search update={filterUpdate} />
 
-                <h1 class="user-title">User Directory List</h1>
-
-
-            <Search update={filterUpdate}/>
-            
-            <Container fluid>
-
-                <Row>
-                    <Col xs={6}>
-                        <h2 className='table-title'>Username</h2>
-                            <div className="tableWrapper">
-                            <table className="table">
-                                
-                                <UserList
-                                    
-                                    data={data}
-                                    update={filterText}
-                                    updateSelected={selectedUpdate}
-                                />
-
-                            </table>
-                        </div>
-                    </Col>
-                    <Col xs={6}>
-                    <ViewUsers 
-                        data={data}
-                        selected={selectedUser}
-                        refreshData={pullInData}
-                            />
-                    </Col>
-                </Row>
-
-            </Container>
-            <Link to='/dashboard'>
-                        <button className='btn'>Dashboard</button>
-            </Link>
-            
-        </div>
-    );
+			<Container fluid>
+				<Row>
+					<Col xs={6}>
+						<h2 className='table-title'>Username</h2>
+						<div className='tableWrapper'>
+							<table className='table'>
+								<UserList
+									data={data}
+									update={filterText}
+									updateSelected={selectedUpdate}
+								/>
+							</table>
+						</div>
+					</Col>
+					<Col xs={6}>
+						<ViewUsers
+							data={data}
+							selected={selectedUser}
+							refreshData={pullInData}
+						/>
+					</Col>
+				</Row>
+			</Container>
+			<Link to='/dashboard'>
+				<button className='btn'>Dashboard</button>
+			</Link>
+		</div>
+	);
 };
 
-export default List; 
+export default List;
--- client/src/views/Login/Login.js
+++ client/src/views/Login/Login.js
@@ -1,104 +1,128 @@
-import React, {useState} from 'react'
-import {Button, Container, Col, Row, Alert, Form, Grid, Jumbotron, Modal} from 'react-bootstrap'
-import './Login.css'
+import React, { useState } from 'react';
+import {
+	Button,
+	Container,
+	Col,
+	Row,
+	Alert,
+	Form,
+	Grid,
+	Jumbotron,
+	Modal,
+} from 'react-bootstrap';
+import './Login.css';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
-const tokenManager = require('../../tokenManager')
+const tokenManager = require('../../tokenManager');
 
 const Login = (props) => {
+	const [email, setEmail] = useState('');
+	const [pass, setPass] = useState('');
+	const [errShow, setErrShow] = useState(false);
+	const [errMsg, setErrMsg] = useState('');
 
-    const [email, setEmail] = useState('');
-    const [pass, setPass] = useState('');
-    const [errShow, setErrShow] = useState(false);
-    const [errMsg, setErrMsg] = useState('')
+	function close() {
+		setErrShow(false);
+	}
 
-    function close() {
-        setErrShow(false);
-    }
+	// Logging in
+	const submit = (event) => {
+		event.preventDefault();
 
-    // Logging in
-    const submit = (event) => {
-        event.preventDefault();
-        
-        axios.post('/users/login', {
-            username: email,
-            pw: pass
-        }).then(response => {
-            console.log(response.data)
-            if(response.data.success === 1) {
+		axios
+			.post('/users/login', {
+				username: email,
+				pw: pass,
+			})
+			.then((response) => {
+				console.log(response.data);
+				if (response.data.success === 1) {
+					// Successful Login
+					const token = response.data.token;
+					axios.defaults.headers.common.token = token;
+					tokenManager.setToken(token);
 
-                // Successful Login
-                const token = response.data.token;
-                axios.defaults.headers.common.token = token;
-                tokenManager.setToken(token);
+					console.log(tokenManager.getCurrentUser());
 
-                console.log(tokenManager.getCurrentUser())
+					props.setCurrUser(tokenManager.getCurrentUser());
 
-                props.setCurrUser(tokenManager.getCurrentUser())
+					props.history.push('/dashboard');
+				} else {
+					setErrMsg(response.data.message);
+					setErrShow(true);
+				}
+			});
+	};
 
-                props.history.push('/dashboard')
+	const onEmailChange = (event) => {
+		setEmail(event.target.value);
+	};
 
-            }
-            else {
-                setErrMsg(response.data.message);
-                setErrShow(true)
-            }
-        })
+	const onPassChange = (event) => {
+		setPass(event.target.value);
+	};
 
-    }
+	return (
+		<Container>
+			<Row className='justify-content-center'>
+				<Col xs={9} md={6}>
+					<div className='landing-title-container'>
+						<h1 className='landing-title'>Sign In</h1>
+					</div>
 
-    const onEmailChange = (event) => {
-        setEmail(event.target.value);
-    }
+					<Form onSubmit={submit}>
+						<Form.Group controlId='formBasicEmail'>
+							<Form.Label style={{ fontSize: '22px' }}>
+								Email address
+							</Form.Label>
+							<Form.Control
+								style={{ fontSize: '18px' }}
+								onChange={onEmailChange}
+								required
+								type='email'
+								placeholder='Enter email'
+							/>
+						</Form.Group>
 
-    const onPassChange = (event) => {
-        setPass(event.target.value);
-    }
+						<Form.Group controlId='formBasicPassword'>
+							<Form.Label style={{ fontSize: '22px' }}>
+								Password
+							</Form.Label>
+							<Form.Control
+								style={{ fontSize: '18px' }}
+								onChange={onPassChange}
+								required
+								type='password'
+								placeholder='Password'
+							/>
+						</Form.Group>
 
-    return (
-        <Container>
-            <Row className='justify-content-center'>
-            <Col xs={9} md={6}>
+						<Button
+							block
+							variant='outline-primary'
+							type='submit'
+							style={{ fontSize: '18px' }}
+						>
+							Login
+						</Button>
+					</Form>
+				</Col>
+			</Row>
 
-            <div className='landing-title-container'>
-            <h1 className='landing-title'>Sign In</h1>
-            </div>
+			<div className='create-account-container'>
+				<Link to='/register' style={{ fontSize: '18px' }}>
+					Register
+				</Link>
+			</div>
 
-            <Form onSubmit={submit}>
-            <Form.Group controlId="formBasicEmail">
-                <Form.Label style={{fontSize: '22px'}}>Email address</Form.Label>
-                <Form.Control style={{fontSize: '18px'}} onChange={onEmailChange} required type="email" placeholder="Enter email" />
-            </Form.Group>
-
-            <Form.Group controlId="formBasicPassword">
-                <Form.Label style={{fontSize: '22px'}}>Password</Form.Label>
-                <Form.Control style={{fontSize: '22px'}} onChange={onPassChange} required type="password" placeholder="Password" />
-            </Form.Group>
-            
-            <Button block variant="outline-primary" type="submit" style={{fontSize: '18px'}}>
-                Login
-            </Button>
-            </Form>
-            </Col>
-            </Row>
-
-            <div className='create-account-container'>
-                <Link to='/register' style={{fontSize: '18px'}}>Register</Link>
-            </div>
-
-            <Modal show={errShow} onHide={close}>
-                <Modal.Body>
-                    {errMsg}
-                </Modal.Body>
-                <Modal.Footer>
-                    <Button onClick={close}>Close</Button>
-                </Modal.Footer>
-            </Modal>
-            
-
-        </Container>
-    )
-
-}
+			<Modal show={errShow} onHide={close}>
+				<Modal.Body>{errMsg}</Modal.Body>
+				<Modal.Footer>
+					<Button onClick={close}>Close</Button>
+				</Modal.Footer>
+			</Modal>
+		</Container>
+	);
+};
 
 export default Login;
